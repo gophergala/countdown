2b9a598d77d46b290a752ea26716b037f5951c84
Michael Wagner <mitch.wagna@gmail.com>
2015-01-25 17:56:20 +0100

    Make the output configurable

--- main.go
+++ main.go
@@ -1,21 +1,30 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"math"
 	"os"
+	"strings"
 	"time"
 )
 
+var (
+	fromSecondsFlag = flag.Bool("s", false, "-s")
+	fromMinutesFlag = flag.Bool("m", false, "-m")
+	fromHoursFlag   = flag.Bool("h", false, "-h")
+	fromDaysFlag    = flag.Bool("d", false, "-d")
+)
+
 func main() {
-	deadline := os.Args[1]
-	fmt.Printf("deadline: %q\n", deadline)
+	flag.Parse()
+	deadline := flag.Arg(0)
 	date, err := time.Parse(time.RFC3339, deadline)
 	if err != nil {
 		fmt.Fprintf(os.Stdout, "Error: %T{%v}\n", err, err)
 		fmt.Fprintf(os.Stdout, "Deadline has to be in RFC3339 format, i.e.: '%s'\n", time.RFC3339)
 		os.Exit(1)
 	}
+	dformat := getFormat()
 	ticker := time.Tick(1 * time.Second)
 	for t := range ticker {
 		remaining := date.Sub(t)
@@ -23,6 +32,94 @@ func main() {
 			fmt.Fprintf(os.Stdout, "\n>>> Deadline reached. Exiting.\n")
 			os.Exit(0)
 		}
-		fmt.Fprintf(os.Stdout, "\r>>> T minus %d seconds", int(math.Ceil(remaining.Seconds())))
+		timeString := format(dformat, remaining)
+		template := fmt.Sprintf("%%%ds\r>>> T minus %%s", len(timeString))
+		fmt.Fprintf(os.Stdout, template, "", timeString)
 	}
+}
+
+func getFormat() durationFormat {
+	if *fromSecondsFlag {
+		return fromSeconds
+	}
+	if *fromMinutesFlag {
+		return fromMinutes
+	}
+	if *fromHoursFlag {
+		return fromHours
+	}
+	if *fromDaysFlag {
+		return fromDays
+	}
+	return Full
+}
+
+type durationFormat int
+
+const (
+	fromSeconds durationFormat = iota << 1
+	fromMinutes
+	fromHours
+	fromDays
+	Full
+)
+
+const (
+	Day  = 24 * time.Hour
+	Year = 365 * Day
+)
+
+func format(typ durationFormat, d time.Duration) string {
+	format := func(x time.Duration, typ string) string {
+		plural := "s"
+		if x == 1 {
+			plural = ""
+		}
+		return fmt.Sprintf("%d %s%s", x, typ, plural)
+	}
+	var segments []string
+	seconds := (d / time.Second) % 60
+	if typ == fromSeconds {
+		segments = append(segments, format((d/time.Second), "second"))
+		return reverseJoin(segments, ", ")
+	} else {
+		segments = append(segments, format(seconds, "second"))
+	}
+	minutes := (d / time.Minute) % 60
+	if typ == fromMinutes {
+		segments = append(segments, format((d/time.Minute), "minute"))
+		return reverseJoin(segments, ", ")
+	} else {
+		segments = append(segments, format(minutes, "minute"))
+	}
+	if minutes != (d / time.Minute) {
+		hours := (d / time.Hour) % 24
+		if typ == fromHours {
+			segments = append(segments, format((d/time.Hour), "hour"))
+			return reverseJoin(segments, ", ")
+		} else {
+			segments = append(segments, format(hours, "hour"))
+		}
+		if hours != (d / time.Hour) {
+			days := (d / Day) % 365
+			if typ == fromDays {
+				segments = append(segments, format((d/Day), "day"))
+				return reverseJoin(segments, ", ")
+			} else {
+				segments = append(segments, format(days, "day"))
+			}
+			if days != (d / Day) {
+				years := d / Year
+				segments = append(segments, format(years, "year"))
+			}
+		}
+	}
+	return reverseJoin(segments, ", ")
+}
+
+func reverseJoin(array []string, separator string) string {
+	for i, j := 0, len(array)-1; i < j; i, j = i+1, j-1 {
+		array[i], array[j] = array[j], array[i]
+	}
+	return strings.Join(array, ", ")
 }
